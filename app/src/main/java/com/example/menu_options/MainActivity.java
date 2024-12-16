be7259b083feedb9badced9c711e06ab53441778
adir1 <aa7381@bs.amalnet.k12.il>
2024-12-16 17:57:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/menu_options/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.menu_options;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText firstnum , secnum ;
+    TextView sum ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        firstnum = findViewById(R.id.firstnum);
+        secnum = findViewById(R.id.secnum);
+        sum = findViewById(R.id.sum);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
