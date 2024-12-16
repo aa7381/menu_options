cdfeca6aa8c0681ffea4f661fc44ba08f6704e35
ADIR <aa7381@bs.amalnet.k12.il>
2024-12-16 18:50:24 +0200

    2

--- app/src/main/java/com/example/menu_options/MainActivity.java
+++ app/src/main/java/com/example/menu_options/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.menu_options;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +13,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     EditText firstnum , secnum ;
     TextView sum ;
@@ -30,5 +32,49 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
     }
+    public boolean onOptionsItemSelected(MenuItem menu)
+    {
+        String num1 = firstnum.getText().toString();
+        String num2 = secnum.getText().toString();
 
-}
+        if(num1.isEmpty() | num2.isEmpty() )
+        {
+            Toast.makeText(this, "pls enter the two num", Toast.LENGTH_SHORT).show();
+            return super.onOptionsItemSelected(menu);
+        }
+        else if(num1.equals("-")| num1.equals(".") | num1.equals("-.") |num1.equals("+") | num1.equals("+.") | num2.equals("-") |num2.equals(".") | num2.equals("-.") |num2.equals("+") | num2.equals("+."))
+        {
+            Toast.makeText(this, "wrong input pls enter only numbers", Toast.LENGTH_SHORT).show();
+            return super.onOptionsItemSelected(menu);
+        }
+        double num11 = Double.parseDouble(num1);
+        double num22 = Double.parseDouble(num2);
+        double result = 0;
+
+        int itemId = menu.getItemId();
+
+        if (itemId == R.id.menuPlus) {
+            result = num11 + num22;
+        } else if (itemId == R.id.menuMinus) {
+            result = num11 - num22;
+        } else if (itemId == R.id.menuKefel) {
+            result = num11 * num22;
+        } else if (itemId == R.id.menuHiluk) {
+            if (num22 != 0) {
+                result = num11 / num22;
+            } else {
+                Toast.makeText(this, "a number cannot be divided by zero", Toast.LENGTH_SHORT).show();
+                return super.onOptionsItemSelected(menu);
+            }
+        } else if (itemId == R.id.menuclear)
+        {
+            firstnum.setText("");
+            secnum.setText("");
+            sum.setText("");
+            return true;
+        }
+
+        sum.setText(String.valueOf(result));
+        return super.onOptionsItemSelected(menu);
+    }
+    }
